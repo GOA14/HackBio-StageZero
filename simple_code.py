c094582409970be53a4dcf3f3a41352211f8011b
GRACE ADELOYE <114221648+GOA14@users.noreply.github.com>
2026-01-21 07:30:33 +0100

    Delete simple_code.py

--- simple_code.py
+++
@@ -1,5 +0,0 @@
-name <- "Grace Adeloye"
-affilation <- "Masters Student at Institute of Genomics and Global Health (Redeemers UNiversity)"
-gene <- "BRCA1"
-organism <- "Humans"
-paste("Hi, my name is", name, "a", affilation, ".", "My favourite gene is ", gene, "in", organism, ".")
