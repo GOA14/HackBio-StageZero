3d087feae0634fe05fde0184ae13d7347b230d22
GRACE ADELOYE <114221648+GOA14@users.noreply.github.com>
2026-01-21 15:11:21 +0100

    Change paste to print for output in simple_code.R

--- simple_code.R
+++ simple_code.R
@@ -2,4 +2,4 @@ name <- "Grace Adeloye"
 affilation <- "Masters Student at Institute of Genomics and Global Health (Redeemers UNiversity)"
 gene <- "BRCA1"
 organism <- "Humans"
-paste("Hi, my name is", name, "a", affilation, ".", "My favourite gene is ", gene, "in", organism, ".")
+print(paste("Hi, my name is", name, "a", affilation, ".", "My favourite gene is ", gene, "in", organism, "."))
